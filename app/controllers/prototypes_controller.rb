386b9fba4fb572b8f7c2cc46d87e24a3ac3d8947
mn25md <mn25md@gmail.com>
2024-08-26 12:37:19 +0900

    修正2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-
+  before_action :move_to_index, except: [:index, :show]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -56,5 +56,11 @@ class PrototypesController < ApplicationController
 
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: :show
-
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      redirect_to root_path
+    end
   end
 end
